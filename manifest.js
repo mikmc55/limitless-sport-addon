ebaf4c614a635937052f386cf25dfeb5c919a31b
greg84-hub <59282468+greg84-hub@users.noreply.github.com>
2023-12-29 21:57:19 +0000

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,7 +1,7 @@
 module.exports={
     id: "org.community.homescreen",
     version: "1.0.1",
-    name: "limitless IPTV sports addon",
+    name: "LimitLess IPTV sports addon",
     logo: "https://dl.strem.io/addon-logo.png",
     description: "This addon brings sports from limitless.",
     types: ["movie", "series","tv","channel"],
