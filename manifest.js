378e58052271fccced98f2275f96a64c830dea59
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-03-05 13:30:33 +0000

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,11 +1,11 @@
 module.exports={
     id: "org.community.homescreen",
     version: "1.0.1",
-    name: "HY USA sports addon",
+    name: "HY IPTV sports addon",
     logo: "https://raw.githubusercontent.com/mikmc55/stremio-ptiptv/master/h_y-high-resolution-logo-transparent.png",
     description: "This addon brings sports from limitless.",
     types: ["movie", "series","tv","channel"],
-    background: "https://i.pinimg.com/736x/cc/8c/1b/cc8c1bd5912b77e7563fd6d46aa7c92b.jpg",
+    background: "https://images.webapi.gc.celticfc.com/76670380-b499-11ea-9b61-111ec6b5da92.jpg",
     resources: ["movie","series","tv"],
     catalogs: [],
     idPrefixes: ["yiptv:"],
