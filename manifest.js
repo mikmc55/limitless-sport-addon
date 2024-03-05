c072dd7af6eb4ab959d8ec3ce5f44c7f4766dd35
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-03-05 13:31:17 +0000

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -5,7 +5,7 @@ module.exports={
     logo: "https://raw.githubusercontent.com/mikmc55/stremio-ptiptv/master/h_y-high-resolution-logo-transparent.png",
     description: "This addon brings sports from limitless.",
     types: ["movie", "series","tv","channel"],
-    background: "https://images.webapi.gc.celticfc.com/76670380-b499-11ea-9b61-111ec6b5da92.jpg",
+    background: "https://i.pinimg.com/736x/cc/8c/1b/cc8c1bd5912b77e7563fd6d46aa7c92b.jpg",
     resources: ["movie","series","tv"],
     catalogs: [],
     idPrefixes: ["yiptv:"],
