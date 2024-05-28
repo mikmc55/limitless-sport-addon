558c37ff5bad95863e443577b7c60ebe53d237ff
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-05-28 07:42:52 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -5,7 +5,7 @@ module.exports={
     logo: "https://raw.githubusercontent.com/mikmc55/stremio-ptiptv/master/h_y-high-resolution-logo-transparent.png",
     description: "This addon brings VOD from streams",
     types: ["movie", "series","tv","channel"],
-    background: "https://mediafusion.elfhosted.com/static/images/mediafusion-elfhosted-logo.png",
+    background: "https://raw.githubusercontent.com/mikmc55/limitless-sport-addon/mediafusion/mediafusion-elfhosted-logo.png",
     resources: ["movie","series","tv"],
     catalogs: [],
     idPrefixes: ["yiptv:"],
