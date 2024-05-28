24c5b910ae5f2c6a2b5630ecf3f7864b973963e4
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-05-28 07:55:26 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,7 +2,7 @@ module.exports={
     id: "org.community.homescreen",
     version: "1.0.1",
     name: "Mediafusion VOD addon",
-    logo: "https://raw.githubusercontent.com/mikmc55/stremio-ptiptv/master/h_y-high-resolution-logo-transparent.png",
+    logo: "https://raw.githubusercontent.com/mikmc55/limitless-sport-addon/mediafusion/mediafusion-elfhosted-logo.png",
     description: "This addon brings VOD from streams",
     types: ["movie", "series","tv","channel"],
     background: "https://raw.githubusercontent.com/mikmc55/limitless-sport-addon/mediafusion/mediafusion-elfhosted-logo.png",
